82d43102b785e6109ca3fff99319db12038b286c
Hugo Mendes <fhugomp@gmail.com>
2025-07-07 22:50:45 -0300

    alterando linhas

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,6 @@ import json
 from datetime import date, time, datetime
 from decimal import Decimal
 
-class CustomJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, (datetime, date, time)):
-            return obj.isoformat()
-        if isinstance(obj, Decimal):
-            return float(obj)
-        return super().default(obj)
-
-
 def create_app():
     app = Flask(__name__)
     app.json_encoder = CustomJSONEncoder
