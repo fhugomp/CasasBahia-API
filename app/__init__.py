e19eb16d945e8df6f1f6fa407baff813f1b6ac1e
Hugo Mendes <fhugomp@gmail.com>
2025-07-08 17:01:15 -0300

    Redirecionamento da page home pra page api

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 def create_app():
     """Cria e configura uma instância do aplicativo Flask."""
@@ -7,5 +7,15 @@ def create_app():
     # Registra o Blueprint que contém as rotas da API
     from .routes import api
     app.register_blueprint(api, url_prefix='/api')
+
+    # --- ROTA DE REDIRECIONAMENTO ---
+    # Esta rota captura acessos à raiz do site (ex: https://casasbahia-api.onrender.com/)
+    @app.route('/')
+    def index_redirect():
+        # E redireciona para a página home da sua API ('/api/')
+        # url_for('api.home') encontra dinamicamente o link para a função 'home'
+        # dentro do blueprint 'api'.
+        return redirect(url_for('api.home'))
+    # --------------------------------
 
     return app
