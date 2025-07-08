ae00c3cc187621da2f85176862449b83c1875351
Hugo Mendes <fhugomp@gmail.com>
2025-07-07 22:09:21 -0300

    Criando o script de criaçao do app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,27 @@
+# app/__init__.py
+
 from flask import Flask
+import json
+from datetime import date, time, datetime
+from decimal import Decimal
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        # --- ADICIONE ESTA LINHA PARA DEPURAR ---
+        print(f"DEBUG: CustomJSONEncoder está processando um objeto do tipo: {type(obj)}")
+        # -----------------------------------------
+
+        if isinstance(obj, (datetime, date, time)):
+            return obj.isoformat()
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super().default(obj)
+
 
 def create_app():
-    """Cria e configura uma instância do aplicativo Flask."""
     app = Flask(__name__)
-
-    # Registra o Blueprint que contém as rotas da API
+    app.json_encoder = CustomJSONEncoder
+    
     from .routes import api
     app.register_blueprint(api, url_prefix='/api')
 
